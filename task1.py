eccc799e61f3d455637a8bf8fef3166b449e8f9a
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-07 18:13:16 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,4 +1,7 @@
 from read_write import write
+from pyspark.sql.functions import col
+title_basics.printSchema()
+title_basics.select([col(c) for c in title_basics.columns[:5]]).show(5)
 
 
 def task1(df):
