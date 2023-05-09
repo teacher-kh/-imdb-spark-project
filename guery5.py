cde8ebe7cdbc2a7ca3e352a598eeddb2b7a0fc12
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-09 15:56:35 +0300

    Update guery5.py

--- guery5.py
+++ guery5.py
@@ -1,4 +1,7 @@
 from pyspark.sql.functions import count
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 adult_counts = ratings.filter(ratings.isAdult == 1).groupBy("region").agg(count("*").alias("adult_count"))
 
