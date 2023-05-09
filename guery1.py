d39f41144de24c1f2ac819755aaa0ec5866e858d
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-09 15:51:18 +0300

    Update guery1.py

--- guery1.py
+++ guery1.py
@@ -1,4 +1,6 @@
-from pyspark.sql.functions import col
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 ukr_titles = df.filter(col("language") == "ukr").select("title")
 ukr_titles.show()
