05698b124bc2f02e7b4a80668b802f999d44eed8
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-09 15:56:10 +0300

    Update guery2.py

--- guery2.py
+++ guery2.py
@@ -1,4 +1,7 @@
 from pyspark.sql.functions import year
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 names_19th_century = name_basics.filter((year(name_basics.birthYear) >= 1800) & (year(name_basics.birthYear) < 1900))
 
