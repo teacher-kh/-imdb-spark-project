4381ce18617cc842fd82462962b0d46dc7dc5c88
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-09 15:56:42 +0300

    Update guery6.py

--- guery6.py
+++ guery6.py
@@ -1,4 +1,7 @@
 from pyspark.sql.functions import count
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 episode_counts = title_basics.filter(title_basics.titleType == 'tvSeries') \
                              .groupBy('parent_tconst') \
