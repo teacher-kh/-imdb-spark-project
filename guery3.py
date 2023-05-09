1deb84ef666fa9e8a3d65a75a076a3906e83c011
teacher-kh <95315351+teacher-kh@users.noreply.github.com>
2023-05-09 15:56:18 +0300

    Update guery3.py

--- guery3.py
+++ guery3.py
@@ -1,4 +1,7 @@
 from pyspark.sql.functions import col
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 long_movies = movies.filter(col('duration') > 120)
 long_movie_titles = long_movies.select('title').collect()
